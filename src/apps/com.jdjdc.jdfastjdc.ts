69fe1f3a2816606e9cb7c5bc71b59b0b8bf5a118
qingya6592 <qingyawilliam@gmail.com>
2026-03-30 19:38:33 +0800

    feat: 新增京东特价版弹窗规则

---
+++ src/apps/com.jdjdc.jdfastjdc.ts
@@ -0,0 +1,19 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.jd.jdfastsjd',
+  name: '奶酪单词',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-弹窗广告',
+      desc: '点击右上角关闭按钮',
+      rules: [
+        {
+          matches: '[vid="fl_container"] > ImageView[index=1]',
+          snapshotUrls: 'https://i.gkd.li/i/26347792',
+        },
+      ],
+    },
+  ],
+});
